298cd583712556c97e37eeec77ba98038c46961d
himanshu <akanku1234kr@gmail.com>
2024-11-06 07:12:55 +0530

    excelent first look

--- src/components/VideoBanner/index.tsx
+++ src/components/VideoBanner/index.tsx
@@ -1,10 +1,10 @@
 import useWindowResize from "../../hooks/useWindowResize";
 import bannerVideo from "../../assets/Video/banner1.mp4";
 import bannerVideoMobile from "../../assets/Video/mobileBanner.mp4";
-import video1 from '../../assets/Video/1.mp4';
-import video2 from '../../assets/Video/2.mp4';
-import video3 from '../../assets/Video/3.mp4';
-import React, { useEffect } from 'react';
+import video1 from "../../assets/Video/1.mp4";
+import video2 from "../../assets/Video/2.mp4";
+import video3 from "../../assets/Video/3.mp4";
+import React, { useEffect } from "react";
 const VideoBanner = () => {
   const width = useWindowResize().width;
   const isMobile = width < 768;
@@ -13,105 +13,126 @@ const VideoBanner = () => {
   const videoKey = videoUrl;
 
   useEffect(() => {
-    const link = document.createElement('link');
-    link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css";
+    const link = document.createElement("link");
+    link.href =
+      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css";
     link.rel = "stylesheet";
     document.head.appendChild(link);
 
     return () => {
-      document.head.removeChild(link); 
+      document.head.removeChild(link);
     };
   }, []);
 
   return (
-    
     <div className="relative h-screen bg-gradient-to-r from-orange-400 via-orange-300 to-red-600">
-         <div className="absolute bottom-0 left-0 w-48 h-48">
-    <div className="w-full h-full bg-orange-300 rounded-" style={{ clipPath: 'circle(90% at 0% 90%)' }}></div>
-    
-  </div>
-  <div className="absolute top-18 right-0 w-32 h-32">
- 
-  <div className="w-full h-full bg-orange-600 rounded-full" style={{ clipPath: 'circle(50% at 50% 50%)' }}></div>
-</div>
-  
-   <main className="flex items-center justify-center min-h-screen">
-      <div className="flex items-center text-center px-8 w-full max-w-8xl pt-20 sm:pt-2 pb-2">
-        <section className="mb-10 mx-8 text-left">
-          <h1  className="lg:text-[40px] text-2xl text-center md:text-4xl md:text-left lg:text-left font-bold text-white mb-8">
-            Unlock Your Imagination  
-  
-             Discover, Learn, and Explore Endless Possibilities
-          </h1>
+      <div className="absolute bottom-0 left-0 w-48 h-48">
+        <div
+          className="w-full h-full bg-orange-300 rounded-"
+          style={{ clipPath: "circle(90% at 0% 90%)" }}
+        ></div>
+      </div>
+      <div className="absolute top-18 right-0 w-32 h-32">
+        <div
+          className="w-full h-full bg-orange-600 rounded-full"
+          style={{ clipPath: "circle(50% at 50% 50%)" }}
+        ></div>
+      </div>
 
-          <div className="flex items-center mb-6">
-            <div className="relative w-full max-w-lg">
-               <input
-                className="flex-grow rounded-full w-full pl-4 pr-12 py-4 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent border border-gray-300 transition duration-200"
-                placeholder="Ex: Coach sportif, nutritionniste..."
-                type="text"
-              />
-             <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black text-white rounded-full w-10 h-10 flex items-center justify-center transition duration-200 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50">
-            <i className="fas fa-search"></i>
-          </button>
-            </div>
-          </div>
+      <main className="flex items-center justify-center min-h-screen">
+        <div className="flex items-center text-center px-8 w-full max-w-8xl pt-20 sm:pt-2 pb-2">
+          <section className="mb-10 mx-8 text-left">
+            <h1 className="lg:text-[40px] text-2xl text-center md:text-4xl md:text-left lg:text-left font-bold text-white mb-8">
+              Unlock Your Imagination Discover, Learn, and Explore Endless
+              Possibilities
+            </h1>
 
-          <p className="text-white mb-4 ">
-          From timeless classics to groundbreaking discoveries, explore endless knowledge.
-          </p>
-          <hr className="max-w-lg" />
-          <p className="text-center text-white max-w-lg">
-          Outstanding &   - <span className="text-green-500">★★★★★</span>   - Amazing Experience!
-</p>
-        </section>
-        <div className="hidden sm:grid grid-cols-1 sm:grid-cols-3 gap-6">
-          <div>
-            <div className="rounded shadow-lg mb-2">
-              <video autoPlay loop muted className="rounded mb-2 mx-auto" width="300">
-              <source src={video1} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+            <div className="flex items-center mb-6">
+              <div className="relative w-full max-w-lg">
+                <input
+                  className="flex-grow rounded-full w-full pl-4 pr-12 py-4 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent border border-gray-300 transition duration-200"
+                  placeholder="Ex: Coach sportif, nutritionniste..."
+                  type="text"
+                />
+                <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black text-white rounded-full w-10 h-10 flex items-center justify-center transition duration-200 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50">
+                  <i className="fas fa-search"></i>
+                </button>
+              </div>
             </div>
-            <div className="bg-white text- rounded shadow-lg p-2">
-              <p className="font-bold">Marie Dupont</p>
-              <p className="text-gray-500">Coach sportif</p>
-              {/* <p className="text-blue-500"></p> */}
-            </div>
-          </div>
 
-          <div>
-            <div className="bg-white text- rounded shadow-lg p-2">
-              <p className="font-bold">Sophie Martin</p>
-              <p className="text-gray-500">Nutritionniste</p>
-              {/* <p className="text-blue-500"></p> */}
+            <p className="text-white mb-4 text-xl">
+              From timeless classics to groundbreaking discoveries, explore
+              endless knowledge.
+            </p>
+            <hr className="max-w-lg" />
+            {/* <p className="text-center text-white max-w-lg">
+              Outstanding & - <span className="text-green-500">★★★★★</span> -
+              Amazing Experience!
+            </p> */}
+          </section>
+          <div className="hidden sm:grid grid-cols-1 sm:grid-cols-3 gap-6">
+            <div>
+              <div className="rounded shadow-lg mb-2">
+                <video
+                  autoPlay
+                  loop
+                  muted
+                  className="rounded mb-2 mx-auto"
+                  width="300"
+                >
+                  <source src={video1} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="bg-white text- rounded shadow-lg p-2">
+                <p className="font-bold">Marie Dupont</p>
+                <p className="text-gray-500">Coach sportif</p>
+                {/* <p className="text-blue-500"></p> */}
+              </div>
             </div>
-            <div className="rounded shadow-lg mt-2">
-              <video autoPlay loop muted className="rounded mb-2 mx-auto" width="300">
-              <source src={video2} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            </div>
-          </div>
 
-          <div>
-            <div className="rounded shadow-lg mb-2">
-              <video autoPlay loop muted className="rounded mb-2 mx-auto" width="300">
-              <source src={video3} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+            <div>
+              <div className="bg-white text- rounded shadow-lg p-2">
+                <p className="font-bold">Sophie Martin</p>
+                <p className="text-gray-500">Nutritionniste</p>
+                {/* <p className="text-blue-500"></p> */}
+              </div>
+              <div className="rounded shadow-lg mt-2">
+                <video
+                  autoPlay
+                  loop
+                  muted
+                  className="rounded mb-2 mx-auto"
+                  width="300"
+                >
+                  <source src={video2} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
             </div>
-            <div className="bg-white text- rounded shadow-lg p-2">
-              <p className="font-bold">Clara Bernard</p>
-              <p className="text-gray-500">Psychologue</p>
-              {/* <p className="text-blue-500"></p> */}
+
+            <div>
+              <div className="rounded shadow-lg mb-2">
+                <video
+                  autoPlay
+                  loop
+                  muted
+                  className="rounded mb-2 mx-auto"
+                  width="300"
+                >
+                  <source src={video3} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="bg-white text- rounded shadow-lg p-2">
+                <p className="font-bold">Clara Bernard</p>
+                <p className="text-gray-500">Psychologue</p>
+                {/* <p className="text-blue-500"></p> */}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-
-
+      </main>
     </div>
   );
 };
